e5d08a05f2d92518cb3d316fcefd6cf163f7bec8
SportsGaming1 <115103781+JamesWicks01@users.noreply.github.com>
2024-04-24 18:22:46 +0100

    Finished Run Workout function

--- client/scripts/runworkout.js
+++ client/scripts/runworkout.js
@@ -2,18 +2,87 @@ function runWorkout() {
   // Retrieving the workout data from local storage
   const workoutData = JSON.parse(localStorage.getItem("activeworkout"));
 
-  // Set initial values
   let currentExerciseIndex = 0;
   let timeRemaining = workoutData.exercises[currentExerciseIndex].duration;
   let timer;
 
   function updateUI() {
-    document.querySelector('WorkoutTitle').innerText = workoutData.title;
-    document.querySelector('currentExerciseName').innerText = workoutData.exercises[currentExerciseIndex].name;
-    document.querySelector('timeRemaining').innerText = 'Time Remaining: ' + timeRemaining + 's';
-    document.querySelector('nextExerciseName').innerText = workoutData.exercises[currentExerciseIndex + 1] ? workoutData.exercises[currentExerciseIndex + 1].name : 'End of Workout';
-    document.querySelector('progress').style.width = ((workoutData.exercises[currentExerciseIndex].duration - timeRemaining) / workoutData.exercises[currentExerciseIndex].duration) * 100 + '%';
-
+    document.querySelector("#WorkoutTitle").textContent = workoutData.title;
+    document.querySelector("#currentExerciseName").textContent =
+      workoutData.exercises[currentExerciseIndex].name;
+    document.querySelector("#timeRemaining").textContent =
+      "Time Remaining: " + timeRemaining + "s";
+    document.querySelector("#nextExerciseName").textContent = workoutData
+      .exercises[currentExerciseIndex + 1]
+      ? workoutData.exercises[currentExerciseIndex + 1].name
+      : "End of Workout";
+    document.querySelector("#progress").style.width =
+      ((workoutData.exercises[currentExerciseIndex].duration - timeRemaining) /
+        workoutData.exercises[currentExerciseIndex].duration) *
+        100 +
+      "%";
   }
 
+  // Countdown function
+  function countdown() {
+    timeRemaining--;
+    updateUI();
+
+    if (timeRemaining <= 0) {
+      clearInterval(timer);
+      currentExerciseIndex++;
+      if (currentExerciseIndex < workoutData.exercises.length) {
+        timeRemaining = workoutData.exercises[currentExerciseIndex].duration;
+        timer = setInterval(countdown, 1000);
+      } else {
+        alert("Workout Completed Well Done");
+      }
+    }
+  }
+
+  // Start Workout Function
+  function startWorkout() {
+    updateUI();
+    timer = setInterval(countdown, 1000);
+    console.log("Workout Started");
+  }
+
+  // Pause Workout Function
+  function pauseWorkout() {
+    clearInterval(timer);
+    document.querySelector("#PauseButton").hidden = true;
+    document.querySelector("#ResumeButton").hidden = false;
+    console.log("Workout Paused");
+  }
+
+  // Resume Workout Function
+  function resumeWorkout() {
+    updateUI();
+    timer = setInterval(countdown, 1000);
+    document.querySelector("#PauseButton").hidden = false;
+    document.querySelector("#ResumeButton").hidden = true;
+    console.log("Workout Resumed");
+  }
+
+  // Exit Workout Fucntion
+  function exitWorkout() {
+    clearInterval(timer);
+    currentExerciseIndex = 0;
+    timeRemaining = workoutData.exercises[currentExerciseIndex].duration;
+    updateUI();
+    window.location = "/workout-searcher";
+  }
+
+  // Registering the Buttons in the page
+  document
+    .querySelector("#PauseButton")
+    .addEventListener("click", pauseWorkout);
+  document
+    .querySelector("#ResumeButton")
+    .addEventListener("click", resumeWorkout);
+  document.querySelector("#ExitButton").addEventListener("click", exitWorkout);
+
+  startWorkout();
 }
+
+window.addEventListener("load", runWorkout);
