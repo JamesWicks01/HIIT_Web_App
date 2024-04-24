175a89bbd97182aae66b1cb745bed79e6831ac0f
JamesWicks01 <115103781+JamesWicks01@users.noreply.github.com>
2024-04-24 16:59:28 +0100

    started with the run workout function

--- client/scripts/runworkout.js
+++ client/scripts/runworkout.js
@@ -1,10 +1,19 @@
-function ExitWorkout() {
-  window.location = "/workout-searcher";
-}
+function runWorkout() {
+  // Retrieving the workout data from local storage
+  const workoutData = JSON.parse(localStorage.getItem("activeworkout"));
 
-function init() {
-  const ExitButton = document.querySelector("#ExitButton");
-  ExitButton.addEventListener("click", ExitWorkout);
-}
+  // Set initial values
+  let currentExerciseIndex = 0;
+  let timeRemaining = workoutData.exercises[currentExerciseIndex].duration;
+  let timer;
 
-window.addEventListener("load", init);
+  function updateUI() {
+    document.querySelector('WorkoutTitle').innerText = workoutData.title;
+    document.querySelector('currentExerciseName').innerText = workoutData.exercises[currentExerciseIndex].name;
+    document.querySelector('timeRemaining').innerText = 'Time Remaining: ' + timeRemaining + 's';
+    document.querySelector('nextExerciseName').innerText = workoutData.exercises[currentExerciseIndex + 1] ? workoutData.exercises[currentExerciseIndex + 1].name : 'End of Workout';
+    document.querySelector('progress').style.width = ((workoutData.exercises[currentExerciseIndex].duration - timeRemaining) / workoutData.exercises[currentExerciseIndex].duration) * 100 + '%';
+
+  }
+
+}
